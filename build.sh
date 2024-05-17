8385ca354e152180a3c1e021a09dff667b52d356
nu1bit <1857773+nu1bit@users.noreply.github.com>
2024-05-17 20:32:54 +0100

    Testing on smaller base image

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install @base-x @standard @kde akmod-nvidia code firefox{,-langpacks} flatpak git google-noto-sans-cjk-fonts gwenview htop iwlwifi-mvm-firmware kcalc keepassxc krita libavcodec-freeworld mesa-va-drivers-freeworld mpv neofetch NetworkManager-wifi nmap nvtop okular plymouth-kcm snapper speedtest-cli strace tlp vim virt-viewer xorg-x11-drv-nvidia-cuda
+#rpm-ostree install @base-x @standard @kde akmod-nvidia code firefox{,-langpacks} flatpak git google-noto-sans-cjk-fonts gwenview htop iwlwifi-mvm-firmware kcalc keepassxc krita libavcodec-freeworld mesa-va-drivers-freeworld mpv neofetch NetworkManager-wifi nmap nvtop okular plymouth-kcm snapper speedtest-cli strace tlp vim virt-viewer xorg-x11-drv-nvidia-cuda
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable tlp
-systemctl set-default graphical.target
+#systemctl enable tlp
+#systemctl set-default graphical.target
