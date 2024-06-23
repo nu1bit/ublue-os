6a34382a29ba95f7bbd0a8554e04b85d8bf67679
nu1bit <1857773+nu1bit@users.noreply.github.com>
2024-06-23 10:38:50 +0100

    Full list of packages to remove

--- build.sh
+++ build.sh
@@ -4,50 +4,48 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+### Compile list of unnecessary packages from base image
+REMOVE_PACKAGES=""
+for pkgs in \
+ImageMagick LibRaw ModemManager NetworkManager-{bluetooth,wwan} \
+anthy antiword apr atheros avif \
+b43 braille brcmfmac brl \
+c-ares cirrus \
+default-fonts-{other,am,ar,as,be,bg,bn,bo,br,chr,dv,dz,el,eo,eu,fa,gu,he,hi,hy,ia,iu,ka,km,kn,ku} \
+default-fonts-{lo,mai,ml,mni,mr,my,nb,ne,nn,nr,nso,or,pa,ru,sat,si,ss,ta,te,th,tn,ts,uk,ur,ve,vi,xh,yi,zu} \
+espeak \
+fedora-{bookmarks,chromium-config,flathub-remote} flite fpaste fwupd-plugin-modem fxload \
+gdouros google-noto-{naskh,sans,serif} gstreamer1-plugins-good-qt \
+heif hfsplus hyperv \
+ibus-{authy,hangul,libpinyin,libzhuyin,m17n} intel \
+jomolhari just \
+kasumi kyotocabinet \
+liblouis libpinyin libva-intel libzhuyin lrzsz \
+madan mpage mtools \
+nilfs nxpwireless \
+oneVPL open{exr,rgb,sc} orca oversteer \
+paktype paps pcsc python3-{boto,brl,louis,pyatspi,s3transfer,simpleaudio,speechd} \
+qemu qrencode qt5 \
+rit \
+sil solaar speech spice sssd \
+tiwilink tracker \
+vazir virtualbox vmaf; do \
+$REMOVE_PACKAGES=$REMOVE_PACKAGES`rpm -qa --qf "%{NAME}\n" | grep -E '^'$pkgs | tr '\n' ' '`; done
 
-### Install packages
+# Keeping back certain packages as they are core dependancies for other packages
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/ModemManager-glib //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-sans-cjk-fonts //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-sans-math-fonts //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-sans-mono-cjk-vf-fonts //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-sans-mono-vf-fonts //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-sans-symbols-vf-fonts //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-sans-symbols2-fonts //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-sans-vf-fonts//')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-serif-cjk-vf-fonts //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/google-noto-serif-vf-fonts //')
+REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/intel-gmmlib //')
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-#rpm-ostree install @base-x @standard @kde akmod-nvidia code firefox{,-langpacks} flatpak git google-noto-sans-cjk-fonts gwenview htop iwlwifi-mvm-firmware kcalc keepassxc krita libavcodec-freeworld mesa-va-drivers-freeworld mpv neofetch NetworkManager-wifi nmap nvtop okular plymouth-kcm snapper speedtest-cli strace tlp vim virt-viewer xorg-x11-drv-nvidia-cuda
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-#systemctl enable tlp
-#systemctl set-default graphical.target
-
-### Disable SystemD targets/services
-#systemctl disable sshd.service
-
-### Remove unnecessary packages from core OS (mostly for running as or managing containers)
-#REMOVE_PACKAGES=""
-#for pkgs in \
-#NetworkManager-cloud \
-#NetworkManager-team \
-#afterburn \
-#clevis \
-#cloud \
-#console \
-#google \
-#intel \
-#moby \
-#nvidia-container \
-#sssd \
-#systemd-container \
-#teamd \
-#toolbox; do REMOVE_PACKAGES=$REMOVE_PACKAGES`rpm -qa --qf "%{NAME}\n" | grep -E '^'$pkgs | tr '\n' ' '`; done
-
-# sssd-client causes the build to fail, so removing it from list of packages to remove
-#REMOVE_PACKAGES=$(echo "$REMOVE_PACKAGES" | sed -r 's/sssd-client //')
-
-#rpm-ostree override remove $REMOVE_PACKAGES
+rpm-ostree override remove $REMOVE_PACKAGES
 
 # Policy to allow upgrades when new builds are available
 cat << 'EOF' | tee /etc/containers/policy.json
@@ -60,3 +58,6 @@ cat << 'EOF' | tee /etc/containers/policy.json
     }
 }
 EOF
+
+## Post install (if rebasing from Kinoite)
+#flatpak remove -y org.kde.{okular,krdc,kolourpaint,kmines,kmahjongg,kcalc,gwenview,elisa} org.fedoraproject.KDE6Platform
